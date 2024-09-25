0732f465d970107e1941de1ddbd5a1c08f0f0ab2
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-25 23:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-// your code here
+  let a=0;
+	let b=1;
+	let sum=0;
+	for(int i=0;i<num;i++)
+	{
+		sum=a+b;
+		a=b;
+		b=sum;
+	}
+	return sum;
 }
 
 module.exports = fibonacci;
