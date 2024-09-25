f6a2bb57fa9b08607795813b11a2bdebeea653e9
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-25 23:46:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+https://course.acciojob.com/open-web-editor?question=b26d3602-54f2-4e6e-8493-6dba39617f2e
+
+
+
 function fibonacci(num) {
-  let a=0;
-	let b=1;
-	let sum=0;
-	for(let i=2;i<=num;i++)
+	if(num==1)
 	{
+		return 0;
+	}
+	else if(num==2)
+	{
+		return 1;
+	}
+	else
+	{
+     let a=0;
+	  let b=1;
+	  let sum=0;
+	  for(let i=3;i<=num;i++)
+	  {
 		sum=a+b;
 		a=b;
 		b=sum;
-	}
+	  }
 	return sum;
+  }
 }
 
 module.exports = fibonacci;
